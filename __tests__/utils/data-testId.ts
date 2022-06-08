ec903c91eed296e91fc90f3c2422a6e008d5c9c2
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-08 11:55:42 -0300

    tests: implement tests for Footer component

--- __tests__/utils/data-testId.ts
+++ __tests__/utils/data-testId.ts
@@ -2,3 +2,4 @@ export const dropdownBtnOpen = 'dropdown-btn-open';
 export const dropdownBtnClose = 'dropdown-btn-close';
 export const dropdownMenu = 'dropdown-menu';
 export const navBarMenu = 'navbar-menu';
+export const footer = 'footer';
