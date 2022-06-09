df0f37cd46e43010a58415fc17c4d5d0dca85e14
Diogo San'Anna <diogo.santanna@outlook.com.br>
2022-06-09 13:52:04 -0300

    feat: add picture and text "Desenvolvedor Web FullStack"

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import myPhoto from '../../assets/foto-do-diogo.png';
 
 export function Home() {
   return (
@@ -7,16 +8,17 @@ export function Home() {
       data-testid="home-section"
       className="h-[795px] w-full md:h-[817px] flex justify-center"
     >
-      <div className="grid grid-rows-3 justify-center w-full h-full
-        max-w-[1169px] md:grid-cols-2 md:grid-rows-2">
-        <div className="w-full h-full flex flex-col items-center">
+      <div className={`grid grid-rows-6 justify-center w-full h-full
+        max-w-[1169px] md:grid-cols-2 md:grid-rows-2 lg:grid-rows-3`}>
+        <div className={`row-span-2 w-full flex flex-col items-center justify-between
+          md:row-span-1 lg:row-span-2`}>
           <div
-          className={`flex flex-col items-center w-[328px] mt-[19px]
+          className={`flex flex-col items-center w-[328px] mt-9
             md:w-full md:mt-[135px]`}
           >
             <h1
               className={`text-lemonade-500 text-[3.125rem] font-semibold
-                w-full md:text-[3.6rem] lg:text-[4.8rem]`}
+                w-full md:w-[375px] md:text-[3.6rem] lg:w-full lg:text-[4.8rem]`}
             >
               Olá!
               <br/>
@@ -25,6 +27,21 @@ export function Home() {
               </span> Diogo
             </h1>
           </div>
+            <h3
+              className="text-[1.35rem] mb-4 md:text-2xl lg:text-[34px]"
+            >
+              Desenvolvedor Web FullStack
+            </h3>
+        </div>
+        <div
+          className={`row-span-3 md:row-span-2 md:flex md:justify-end md:items-center
+            lg:row-span-3`}
+        >
+          <img
+            src={myPhoto}
+            alt="foto-do-Diogo"
+            className="w-[324px] md:w-[400px] md:mb-20 lg:w-[500px] lg:mb-24"
+          />
         </div>
       </div>
     </section>
