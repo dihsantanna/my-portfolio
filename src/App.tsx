3c0030da62e1793d263fb8aea8ddebb71f568e1e
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-07-05 17:56:16 -0300

    refactor: code refactor to put site in production as MVP (#4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { DropdownMenu } from './components/DropdownMenu';
+// import { DropdownMenu } from './components/DropdownMenu';
 import { Footer } from './components/Footer';
 import { Home } from './components/Home';
-import { NavBar } from './components/NavBar';
+// import { NavBar } from './components/NavBar';
 
 export function App() {
   return (
     <>
-      <DropdownMenu />
-      <NavBar />
+      {/* <DropdownMenu />
+      <NavBar /> */}
       <Home />
       <Footer />
     </>
